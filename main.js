4fa6618e6451ac73e19e8b2405f2db033759fb40
Hanno Braun <hb@hannobraun.com>
2011-02-18 18:16:55 +0100

    Enhancement: Moved rendering into a main loop.

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 
-function main() {
-	var renderSystem = new RenderSystem( "canvas", 600, 600 );
+var tick = 20;
+
+var renderSystem = null;
+
+var position1 = null;
+var position2 = null;
+
+var images = null;
+
+function init() {
+	renderSystem = new RenderSystem( "canvas", 600, 600 );
 	
-	var position1 = {
+	position1 = {
 		x: 268,
 		y: 268
 	};
-	var position2 = {
+	position2 = {
 		x: 50,
 		y: 50
 	};
 	
-	onLoadDo( [ "black-hole.png", "projectile.png" ], function( images ) {
-		renderSystem.render( [ position1, position2 ], images );
+	onLoadDo( [ "black-hole.png", "projectile.png" ], function( loadedImages ) {
+		images = loadedImages;
+		setTimeout( main, tick );
 	} );
+	
+	
 }
 
 function onLoadDo( imagePaths, fn ) {
@@ -36,4 +48,10 @@ function onLoadDo( imagePaths, fn ) {
 		
 		image.src = imagePaths[ i ];
 	}
+}
+
+function main() {
+	renderSystem.render( [ position1, position2 ], images );
+	
+	setTimeout( main, 20 );
 }
