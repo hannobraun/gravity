dec41a5406a86b4f0fa6277ad8ec176af9e5e943
Hanno Braun <hb@hannobraun.com>
2011-02-18 18:06:10 +0100

    Enhancement: Added the projectile graphic and displayed it on the screen.

--- main.js
+++ main.js
@@ -2,14 +2,38 @@
 function main() {
 	var renderSystem = new RenderSystem( "canvas", 600, 600 );
 	
-	var position = {
+	var position1 = {
 		x: 284,
 		y: 284
-	}
+	};
+	var position2 = {
+		x: 50,
+		y: 50
+	};
+	
+	onLoadDo( [ "black-hole.png", "projectile.png" ], function( images ) {
+		renderSystem.render( [ position1, position2 ], images );
+	} );
+}
 
-	var image = new Image();	
-	image.onload = function() {
-		renderSystem.render( [ position ], [ image ] );
+function onLoadDo( imagePaths, fn ) {
+	
+	var waitingForImages = 0;
+	var images = [];
+	
+	for ( var i = 0; i < imagePaths.length; i++ ) {
+		waitingForImages++;
+		
+		var image = new Image();
+		images.push( image );
+		
+		image.onload = function() {
+			waitingForImages--;
+			if ( waitingForImages == 0 ) {
+				fn( images );
+			}
+		}
+		
+		image.src = imagePaths[ i ];
 	}
-	image.src = "black-hole.png";
 }
