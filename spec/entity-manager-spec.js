2c2e099df37eb569f38c05acecb6d19ddd2673c7
Hanno Braun <hb@hannobraun.com>
2011-02-20 17:38:17 +0100

    Refactoring: Removed some duplication in EntityManager spec.

--- spec/entity-manager-spec.js
+++ spec/entity-manager-spec.js
@@ -1,34 +1,39 @@
 
 describe( "EntityManager", function() {
 
+	var componentX1 = {
+		x: "x1"
+	};
+	var componentX2 = {
+		x: "x2"
+	};
+	var componentY1 = {
+		y: "y1"
+	};
+	var componentY2 = {
+		y: "y2"
+	};
+
 	var entityManager = null;
 	
 	beforeEach( function() {
 		entityManager = new EntityManager();
 		
 		entityManager.defineEntity( "entity1", {
-			component1: {
-				x: "x1"
-			},
-			component2: {
-				y: "y1"
-			}
+			componentX: componentX1,
+			componentY: componentY1
 		} );
 		entityManager.defineEntity( "entity2", {
-			component1: {
-				x: "x2"
-			},
-			component2: {
-				y: "y2"
-			}
+			componentX: componentX2,
+			componentY: componentY2
 		} );
 	} );
 
 	it( "should return all components of a specific type.", function () {
-		var result = entityManager.componentsByType( "component1" );
+		var result = entityManager.componentsByType( "componentX" );
 		
 		expect( result.entities ).toEqual( [ "entity1", "entity2" ] );
-		expect( result.components ).toEqual( [ { x: "x1" }, { x: "x2" } ] );
+		expect( result.components ).toEqual( [ componentX1, componentX2 ] );
 	} );
 	
 	it( "should return only those entities that actually have the specified component.", function() {
@@ -38,7 +43,7 @@ describe( "EntityManager", function() {
 			}
 		} );
 		
-		var result = entityManager.componentsByType( "component1" );
+		var result = entityManager.componentsByType( "componentX" );
 		
 		expect( result.entities ).toEqual( [ "entity1", "entity2" ] );
 		expect( result.components ).toEqual( [ { x: "x1" }, { x: "x2" } ] );
