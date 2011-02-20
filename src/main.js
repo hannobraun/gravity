339897299eaa889ac805caab69ee6a44b1a8ebd1
Hanno Braun <hb@hannobraun.com>
2011-02-20 22:05:39 +0100

    Refactoring: Extracted image loading logic into a separate file.

--- src/main.js
+++ src/main.js
@@ -37,34 +37,12 @@ function init() {
 	} );
 	
 	var imagePaths = entityManager.componentsByType( [ "imagePath" ] );
-	onLoadDo( imagePaths.components[ "imagePath" ], function( loadedImages ) {
+	loadImagesAndDo( imagePaths.components[ "imagePath" ], function( loadedImages ) {
 		for ( var i = 0; i < imagePaths.entities.length; i++ ) {
 			entityManager.addComponentToEntity( "image", loadedImages[ i ], imagePaths.entities[ i ] );
 		}
 		main();
 	} );
-}
-
-function onLoadDo( imagePaths, fn ) {
-	
-	var waitingForImages = 0;
-	var images = [];
-	
-	for ( var i = 0; i < imagePaths.length; i++ ) {
-		waitingForImages++;
-		
-		var image = new Image();
-		images.push( image );
-		
-		image.onload = function() {
-			waitingForImages--;
-			if ( waitingForImages == 0 ) {
-				fn( images );
-			}
-		}
-		
-		image.src = imagePaths[ i ];
-	}
 }
 
 function main() {
